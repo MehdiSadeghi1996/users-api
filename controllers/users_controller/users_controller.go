373e71f97c505aecc804b2947bd42aa66967aebd
Mehdi Sadeghi <m.sadeghi@decoapp.com>
2022-02-18 14:00:08 +0330

    set up service

--- controllers/users_controller/users_controller.go
+++ controllers/users_controller/users_controller.go
@@ -3,10 +3,24 @@ package users_controller
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"users-api/domain/users"
+	"users-api/services"
+	"users-api/utils/errors"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "nop")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "nop")
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,13 @@
 package services
+
+import (
+	"net/http"
+	"users-api/domain/users"
+	"users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return nil, &errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+}
